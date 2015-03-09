e3332784010b5379870e8413180eb7daabc18ca8
Anirudh Bagde <anidev.aelico@gmail.com>
2015-03-09 00:58:05 -0400

    Now sends key release updates, but there is an issue with key repeats in Tk

--- ds.py
+++ ds.py
@@ -3,31 +3,40 @@
 import struct
 import socket as so
 import Tkinter as tk
-from crc16pure import *
+from crc16pure import crc16xmodem
 
 ControlData_format = '<B'
 
-def send_data(up, down, left, right):
-	dir_data = int(up) | (int(down) << 1) | (int(left) << 2) | (int(right) << 3)
+control = {'up': False, 'down': False, 'left': False, 'right': False}
+
+def update_control(k, pressed):
+	k = k.lower()
+	if k in control:
+		control[k] = pressed
+
+def send_data(data):
+	dir_data = int(data['up']) | (int(data['down']) << 1) | (int(data['left']) << 2) | (int(data['right']) << 3)
 	data_bare = struct.pack(ControlData_format, dir_data)
 	crc16 = crc16xmodem(data_bare)
 	data = struct.pack(ControlData_format + 'H', dir_data, crc16)
 	sock.sendto(data, ('192.168.0.2', 6800))
 
-def keypress(event):
+def keyevent(event):
 	k = event.keysym
 	key_text.set(k)
-	send_data(k == 'Up', k == 'Down', k == 'Left', k == 'Right')
+	update_control(k, event.type == '2') # KeyPress = 2
+	send_data(control)
 
 sock = so.socket(so.AF_INET, so.SOCK_DGRAM)
 sock.setsockopt(so.SOL_SOCKET, so.SO_REUSEADDR, 1)
 
 root = tk.Tk()
 root.geometry('500x50')
-label = tk.Label(root, text = "Press arrow keys to transmit", font = ('', 14))
+label = tk.Label(root, text="Press arrow keys to transmit", font=('', 14))
 key_text = tk.StringVar()
-key_label = tk.Label(root, textvariable = key_text, font = ('', 14))
+key_label = tk.Label(root, textvariable=key_text, font=('', 14))
 label.pack()
 key_label.pack()
-root.bind('<KeyPress>', keypress)
+root.bind('<KeyPress>', keyevent)
+root.bind('<KeyRelease>', keyevent)
 root.mainloop()
