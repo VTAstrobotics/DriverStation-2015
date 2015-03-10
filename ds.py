6d510ff48a9b4bda891a3241a18f61e4cf02e335
Anirudh Bagde <anidev.aelico@gmail.com>
2015-03-10 02:36:30 -0400

    Changed tabs to 4 spaces

--- ds.py
+++ ds.py
@@ -14,36 +14,36 @@ key_text = ''
 control = {'up': False, 'down': False, 'left': False, 'right': False}
 
 def update_control(k, pressed):
-	k = k.lower()
-	if k in control:
-		control[k] = pressed
+    k = k.lower()
+    if k in control:
+        control[k] = pressed
 
 def send_data(data):
-	dir_data = int(data['up']) | (int(data['down']) << 1) | (int(data['left']) << 2) | (int(data['right']) << 3)
-	data_bare = struct.pack(ControlData_format, dir_data)
-	crc16 = crc16xmodem(data_bare)
-	data = struct.pack(ControlData_format + 'H', dir_data, crc16)
-	sock.sendto(data, ('192.168.0.2', 6800))
+    dir_data = int(data['up']) | (int(data['down']) << 1) | (int(data['left']) << 2) | (int(data['right']) << 3)
+    data_bare = struct.pack(ControlData_format, dir_data)
+    crc16 = crc16xmodem(data_bare)
+    data = struct.pack(ControlData_format + 'H', dir_data, crc16)
+    sock.sendto(data, ('192.168.0.2', 6800))
 
 def keyevent(event):
-	global key_text
-	k = pygame.key.name(event.key)
-	key_text = k
-	update_control(k, event.type == pygame.KEYDOWN)
-	send_data(control)
+    global key_text
+    k = pygame.key.name(event.key)
+    key_text = k
+    update_control(k, event.type == pygame.KEYDOWN)
+    send_data(control)
 
 def render():
-	screen.fill((223, 223, 223))
+    screen.fill((223, 223, 223))
 
-	help_label = font.render("Press arrow keys to transmit", True, (0, 0, 0))
-	help_rect = help_label.get_rect()
-	help_rect.midtop = (win_size[0] / 2, 4)
-	screen.blit(help_label, help_rect)	
+    help_label = font.render("Press arrow keys to transmit", True, (0, 0, 0))
+    help_rect = help_label.get_rect()
+    help_rect.midtop = (win_size[0] / 2, 4)
+    screen.blit(help_label, help_rect)
 
-	key_label = font.render(key_text, True, (0, 0, 0))
-	key_rect = key_label.get_rect()
-	key_rect.midbottom = (win_size[0] / 2, win_size[1] - 4)
-	screen.blit(key_label, key_rect)
+    key_label = font.render(key_text, True, (0, 0, 0))
+    key_rect = key_label.get_rect()
+    key_rect.midbottom = (win_size[0] / 2, win_size[1] - 4)
+    screen.blit(key_label, key_rect)
 
 sock = so.socket(so.AF_INET, so.SOCK_DGRAM)
 sock.setsockopt(so.SOL_SOCKET, so.SO_REUSEADDR, 1)
@@ -55,12 +55,12 @@ font = pygame.font.Font(None, 22)
 pygame.display.flip()
 
 while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-		elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-			keyevent(event)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+            keyevent(event)
 
-	render()
-	pygame.display.flip()
-	time.sleep(0.01)
+    render()
+    pygame.display.flip()
+    time.sleep(0.01)
