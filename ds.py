36b554db1042ea17793bc091148024a545a25d9e
Anirudh Bagde <anidev.aelico@gmail.com>
2015-05-12 18:32:10 -0400

    Replaced XboxController.py with custom module that supports Xbox and regular HID gamepads

--- ds.py
+++ ds.py
@@ -1,33 +1,37 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# 2015 Python driver station for Astrobotics
+# Should support both python 2 and 3
+
 import time
 import struct
 import socket as so
 from crc16pure import crc16xmodem
-from XboxController import XboxController
-XboxControls = XboxController.XboxControls
+from joyinput import JoyInput, JoyControls
 
 ControlData_format = '<BB'
 
 def clamp(minN, maxN, N):
     return max(min(maxN, N), minN)
 
-def xboxevent(controlId, value):
-    if controlId <= XboxControls.LTRIGGER:
-        # The triggers have range [0, 100] instead of [-100, 100]
+def handler(controlId, value):
+    # Format axis data
+    if controlId <= JoyControls.LTRIGGER:
+        # NOTE: The triggers have range [0, 100] instead of [-100, 100]
         # So the final value that is sent will have range [90, 180] instead of [0, 180]
         value = clamp(0, 180, int((value + 1) * 90))
-    elif controlId == XboxControls.DPAD:
+    # Format dpad data
+    elif controlId == JoyControls.DPAD:
         # DPAD is 4 groups of 2 bits: +Y -Y +X -X
         value = (int(value[0] < 0)) \
               | (int(value[0] > 0) << 2) \
               | (int(value[1] < 0) << 4) \
               | (int(value[1] > 0) << 6)
-    print controlId,value
+    print(controlId,value)
     send_data(controlId, value)
 
-xbox = XboxController(controllerCallBack = xboxevent, joystickNo = 0, deadzone = 0.2, scale = 1, invertYAxis = False)
+joy = JoyInput(joyIndex = 0, deadzone = 0.2, callback = handler)
 
 def send_data(controlId, value):
     data = [controlId, value]
@@ -41,13 +45,13 @@ sock = so.socket(so.AF_INET, so.SOCK_DGRAM)
 sock.setsockopt(so.SOL_SOCKET, so.SO_REUSEADDR, 1)
 
 try:
-    xbox.start()
-    print "Controller started"
+    joy.start()
+    print("Controller started")
     while True:
         time.sleep(10)
 except (KeyboardInterrupt, SystemExit):
-    print "Exiting"
+    print("Exiting")
 finally:
-    xbox.stop()
+    joy.stop()
 
 exit()
---
+++ joyinput.py
@@ -0,0 +1,134 @@
+# Handles joystick/gamepad input
+# Takes care of translating both Xbox and regular
+# USB gamepad controls to a common format
+# -*- coding: utf-8 -*-
+
+import os
+from threading import Thread
+import pygame
+
+class JoyControls(object):
+        LTHUMBX = 0
+        LTHUMBY = 1
+        RTHUMBX = 2
+        RTHUMBY = 3
+        RTRIGGER = 4
+        LTRIGGER = 5
+        A = 6
+        B = 7
+        X = 8
+        Y = 9
+        LB = 10
+        RB = 11
+        BACK = 12
+        START = 13
+        XBOX = 14
+        LTHUMBBTN = 15
+        RTHUMBBTN = 16
+        DPAD = 17
+        L2 = 18
+        R2 = 19
+
+class ControlMapXbox(object):
+    """ Control map for Xbox controllers, should be using
+        the xboxdrv userspace driver """
+        
+    axes = {0: JoyControls.LTHUMBX,
+            1: JoyControls.LTHUMBY,
+            2: JoyControls.RTHUMBX,
+            3: JoyControls.RTHUMBY,
+            4: JoyControls.LTRIGGER,
+            5: JoyControls.RTRIGGER}
+    
+    buttons = {0: JoyControls.A,
+               1: JoyControls.B,
+               2: JoyControls.X,
+               3: JoyControls.Y,
+               4: JoyControls.LB,
+               5: JoyControls.RB,
+               6: JoyControls.BACK,
+               7: JoyControls.START,
+               8: JoyControls.XBOX,
+               9: JoyControls.LTHUMBBTN,
+               10: JoyControls.RTHUMBBTN}
+
+class ControlMapHID(object):
+    """ Control map for regular USB (HID) gamepads """
+    
+    axes = {0: JoyControls.LTHUMBX,
+            1: JoyControls.LTHUMBY,
+            2: JoyControls.RTHUMBX,
+            3: JoyControls.RTHUMBY}
+    
+    buttons = {0: JoyControls.X,
+               1: JoyControls.A,
+               2: JoyControls.B,
+               3: JoyControls.Y,
+               4: JoyControls.LB,
+               5: JoyControls.RB,
+               6: JoyControls.L2,
+               7: JoyControls.R2,
+               8: JoyControls.BACK,
+               9: JoyControls.START,
+               10: JoyControls.LTHUMBBTN,
+               11: JoyControls.RTHUMBBTN}
+
+class JoyInput(Thread):
+    def __init__(self, joyIndex=0, deadzone=0.1, callback=None):
+        Thread.__init__(self)
+        self.joyIndex = joyIndex
+        self.deadzone = deadzone
+        self.callback = callback
+        self.running = False
+        
+        # Setup pygame
+        # Pygame must have a display created to use joysticks,
+        # so use the dummy video driver to avoid needing a GUI
+        os.environ['SDL_VIDEODRIVER'] = 'dummy'
+        pygame.init()
+        pygame.display.set_mode((1, 1))
+        # Initialize joystick
+        pygame.joystick.init()
+        self.joy = pygame.joystick.Joystick(joyIndex)
+        self.joy.init()
+        
+        # Figure out joystick type and assign appropriate map
+        if 'Xbox' in self.joy.get_name():
+            self.map = ControlMapXbox
+        else:
+            self.map = ControlMapHID 
+    
+    def run(self):
+        self.running = True
+        
+        while(self.running):
+            for event in pygame.event.get():
+                if event.type == pygame.JOYAXISMOTION:
+                    self._handle_axis(event.axis, event.value)
+                elif event.type == pygame.JOYHATMOTION:
+                    self._handle_dpad(event.value)
+                elif event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP:
+                    self._handle_button(event.button, event.type)
+
+    def _handle_axis(self, axis, value):
+        if axis not in self.map.axes:
+            return
+        if abs(value) < self.deadzone:
+            value = 0
+        self._fire(self.map.axes[axis], value)
+        
+    def _handle_dpad(self, value):
+        self._fire(JoyControls.DPAD, value)
+    
+    def _handle_button(self, buttonId, eventType):
+        if buttonId not in self.map.buttons:
+            return
+        value = 1 if eventType == pygame.JOYBUTTONDOWN else 0
+        self._fire(self.map.buttons[buttonId], value)
+        
+    def _fire(self, controlId, value):
+        if self.callback:
+            self.callback(controlId, value)
+        
+    def stop(self):
+        self.running = False
